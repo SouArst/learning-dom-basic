71e66f1ac6a464086a6c98f62a53fbfdb203c621
SouArst <alexsandrorodriguestdb@gmail.com>
2025-01-30 18:40:52 -0300

    working with events

---
+++ 08-working-with-events/index.js
@@ -0,0 +1,17 @@
+function register (ev){
+  console.log(ev)
+  const sectonElement = ev.currentTarget.parentNode
+  const username = sectonElement.children.username.value
+  const password = sectonElement.children.password.value
+  const passwordConfirm = sectonElement.children.passwordConfirm.value
+
+  if (password === passwordConfirm){
+    alert ("User " +username+ " successfully registered")
+  } else {
+    alert ("Passwords don't match")
+  }
+}
+
+const button = document.getElementById('register-button')
+
+button.addEventListener('click', register)
